c5d69f134679fc851d14584ea2edfd0c9b70cb6e
Jon Rood <jon.rood@nrel.gov>
2025-11-20 09:54:41 -0700

    Fix averaging.

--- AMR-Wind/amr-wind-benchmark-gpu-aware.sh
+++ AMR-Wind/amr-wind-benchmark-gpu-aware.sh
@@ -42,7 +42,7 @@ i=1
 for file in $(ls -d1 amr-wind-benchmark* | sort -V); do
     echo "$file"
     grep ^WallClockTime "$file" | awk '{print $NF}' > amr-wind-time-$i.txt
-    ./amr-wind-average.py -f amr-wind-time-$i.txt >> amr-wind-avg.txt
+    python3 amr-wind-average.py -f amr-wind-time-$i.txt >> amr-wind-avg.txt
     rm amr-wind-time-$i.txt
     ((i=i+1))
 done
